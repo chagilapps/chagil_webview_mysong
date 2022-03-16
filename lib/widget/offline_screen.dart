7b38ff04a8a0081587aa3f9596517fe8e73d6ddc
chagilapps <chagilmarketing@gmail.com>
2022-03-16 19:41:12 +0200

    offline screen

--- lib/widget/offline_screen.dart
+++ lib/widget/offline_screen.dart
@@ -7,15 +7,20 @@ class OfflineScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset("assets/icon/icon.png"),
-          Text('אין חיבור לאינטרנט, תוכן האפליקציה יעלה מייד עם חידוש החיבור לאינטרנט',
-          style: TextStyle(),
-          textAlign: TextAlign.center,),
-        ],
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/icon/icon.png",width: MediaQuery.of(context).size.width *.8,),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('אופס, כנראה שאין אצלך חיבור לאינטרנט כרגע..\n יש לנסות שוב.\n\n תוכן האפליקציה יוצג מיד עם חידוש הקליטה במכשירך :)',
+              style: TextStyle(fontSize: 17),
+              textAlign: TextAlign.center,textDirection: TextDirection.rtl,),
+            ),
+          ],
+        ),
       ),
     );
   }
