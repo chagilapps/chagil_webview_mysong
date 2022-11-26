e73dd558b90f2971e6a982fc5e20cb0e3eeba9d8
chagil <chagilmarketing@gmail.com>
2022-11-26 22:30:42 +0200

    change to old build name ++ one build

--- lib/widget/app_button_bar.dart
+++ lib/widget/app_button_bar.dart
@@ -1,38 +1,68 @@
- 
- 
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 import '../app_config.dart';
 
 Widget appButtonBar(_webViewController) => ButtonBar(
-
-          alignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              color: mainAppColor,
-              child: Icon(Icons.arrow_back),
-              onPressed: () {
-                if (_webViewController != null) {
-                  _webViewController!.goBack();
-                }
-              },
-            ),
-            RaisedButton(
-              color: mainAppColor,
-              child: Icon(Icons.arrow_forward),
-              onPressed: () {
-                if (_webViewController != null) {
-                  _webViewController!.goForward();
-                }
-              },
-            ),
-            RaisedButton(
-              child: Icon(Icons.refresh),
-              onPressed: () {
-                if (_webViewController != null) {
-                  _webViewController!.reload();
-                }
-              },
-            ),
-          ],
-        );
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            if (_webViewController != null) {
+              _webViewController!.goBack();
+            }
+          },
+          child: Icon(Icons.arrow_back),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: mainAppColor,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (_webViewController != null) {
+              _webViewController!.goForward();
+            }
+          },
+          child: Icon(Icons.arrow_forward),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: mainAppColor,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (_webViewController != null) {
+              _webViewController!.reload();
+            }
+          },
+          child: Icon(Icons.refresh),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: mainAppColor,
+          ),
+        )
+        // RaisedButton(
+        //   color: mainAppColor,
+        //   child: Icon(Icons.arrow_back),
+        //   onPressed: () {
+        //     if (_webViewController != null) {
+        //       _webViewController!.goBack();
+        //     }
+        //   },
+        // ),
+        // RaisedButton(
+        //   color: mainAppColor,
+        //   child: Icon(Icons.arrow_forward),
+        //   onPressed: () {
+        //     if (_webViewController != null) {
+        //       _webViewController!.goForward();
+        //     }
+        //   },
+        // ),
+        // RaisedButton(
+        //   child: Icon(Icons.refresh),
+        //   onPressed: () {
+        //     if (_webViewController != null) {
+        //       _webViewController!.reload();
+        //     }
+        //   },
+        // ),
+      ],
+    );
